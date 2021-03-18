8382b6f8baee92d0d4fa26ef856a6b409c2c2b7e
Suphada <614259054@webmail.npru.ac.th>
2021-03-18 12:01:11 +0700

    Calendar

---
+++ showdate.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+</head>
+
+<body>
+    <?php
+    include "Calendar.php";
+    $calendar = new Calendar();
+    $month = $_POST("month");
+    $day = $_POST("day");
+    $result = $calendar->Showdate($month, $day);
+    echo "<h2 id='result'>$result</h2>";
+
+
+    ?>
+</body>
+
+</html>
